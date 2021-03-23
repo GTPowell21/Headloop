51f88999bf48badcb48a0677c192ce5ad5431410
GTPowell21 <69421431+GTPowell21@users.noreply.github.com>
2021-03-23 11:09:48 +0000

    Removed example from package script

--- Headloop_package.py
+++ Headloop_package.py
@@ -174,25 +174,3 @@ def design(sense_oligo, antisense_oligo, guide_context, orientation):
             antisense_hl.description = 'Tm difference < 3\u00b0C'
             
     return (sense_hl, antisense_hl)
-
-##Example from the paper: tbx16_AA
-
-#Forward primer
-tbx16_AA_F = 'CTGGTCCAGTGCGTTATTGG'
-
-#Reverse primer
-tbx16_AA_R = 'AGCCAAATGCTTCTTGCTCTTTT'
-
-#Guide sequence and PAM, plus 15 bp downstream sequence
-tbx16_AA_guide = 'CTACAGGACGTACCTGCACCCGGATTCACCAGCGCCCG'
-
-#Orientation of guide with respect to forward primer: same strand = sense, opposite strand = 'antisense'
-tbx16_AA_guide_orientation = 'antisense'
-
-#Run the function
-output = design(tbx16_AA_F, tbx16_AA_R, tbx16_AA_guide, tbx16_AA_guide_orientation)
-
-#Output of the function: note that 'design' returns two primers as SeqRecord objects, with comments on how closely matched
-#the predicated annealing temperatures are. Only one of those primers should be used, to be determined by testing.
-#In this individual case, the antisense headloop primer was used for HL-PCR.
-print('Sense headloop primer:', output[0].seq, '\n', output[0].description, '\n', 'Antisense headloop primer:', output[1].seq, '\n', output[1].description)
